5f8f15b9ee29905cf992173281c5441fc5ffa73b
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-06-08 18:36:10 +0200

    Add switch background on click gender button in General info screen

--- app/src/main/java/com/example/pupil/ReportPetGeneralInfoActivity.kt
+++ app/src/main/java/com/example/pupil/ReportPetGeneralInfoActivity.kt
@@ -3,9 +3,19 @@ package com.example.pupil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import com.example.pupil.model.utilities.PetGender
+import com.example.pupil.model.utilities.PetType
+import kotlinx.android.synthetic.main.activity_report_pet_general_info.*
 import kotlinx.android.synthetic.main.activity_report_pet_type.*
+import kotlinx.android.synthetic.main.activity_report_pet_type.backArrow
+import kotlinx.android.synthetic.main.activity_report_pet_type.nextReportBtn
+import java.util.*
 
 class ReportPetGeneralInfoActivity : AppCompatActivity() {
+
+    var genderTypeFilter: EnumSet<PetGender> = EnumSet.allOf(PetGender::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_pet_general_info)
@@ -15,6 +25,47 @@ class ReportPetGeneralInfoActivity : AppCompatActivity() {
         //Back screen button
         backArrow.setOnClickListener{backBtn()}
     }
+
+
+
+
+    //Button Male and Female function:
+
+    override fun onStart() {
+        super.onStart()
+
+        //Switch pet gender type:
+        genderFemaleTypeView.setOnClickListener{
+            toggleGenderType(PetGender.FEMALE)
+        }
+        genderMaleTypeView.setOnClickListener{
+            toggleGenderType(PetGender.MALE)
+        }
+
+    }
+
+
+    private fun toggleGenderType(gender: PetGender){
+        if(genderTypeFilter.contains(gender)){
+            genderTypeFilter.remove(gender)
+        }else{
+            genderTypeFilter.add(gender)
+        }
+        updateFilterItems()
+    }
+
+    private fun updateFilterItems(){
+        val chooseBackground = { item: LinearLayout, gender: PetGender ->
+            if (genderTypeFilter.contains(gender)) {
+                item.setBackgroundResource(R.drawable.pet_filter_frame_selected)
+            }else{
+                    item.setBackgroundResource(R.drawable.pet_filter_frame)
+                }
+            }
+            chooseBackground(genderMaleTypeView, PetGender.MALE)
+            chooseBackground(genderFemaleTypeView, PetGender.FEMALE)
+        }
+
 
     //Function to back screen
     private fun backBtn(){
